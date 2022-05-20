dcd84c5f351332d211159e022408e63c5504df91
Tremelle Ancrum <tancrum1996@gmail.com>
2022-05-20 10:22:06 -0400

    commit tree 055425f1d131549779b904b9173acbddf464ba0e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function findMatching(array, string) {
+   const newList = array.filter(drivers => drivers.toLowerCase() === string.toLowerCase())
+   return newList
+}
+
+function fuzzyMatch(array, string) {
+    
+}
